0572b8911e6ce75d7d62f335d41400280ca6ef50
zongai <zongai@users.noreply.github.com>
2026-09-08 04:50:13 +0000

    fix: remove AppFontRegistration after dropping bundled fonts

--- IosRss/App.swift
+++ IosRss/App.swift
@@ -3,7 +3,6 @@ import SwiftUI
 @main
 struct IosRssApp: App {
     init() {
-        AppFontRegistration.register()
         Cloud.configure(appId: "hzj3sd6a92mn")
         // 扩大 URL 磁盘缓存，配合 OfflineCache 支持离线读正文/图片
         OfflineCache.configureURLCache()
